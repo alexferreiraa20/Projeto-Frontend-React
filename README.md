a353d283d8ebd1d846cfec33973dee33687ae8bd
Alex <alexferreiraa20@gmail.com>
2022-12-10 11:00:02 -0300

    feito

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Projeto-Frontend-React
+# template-estado-global
+### link do codesandbox
+https://codesandbox.io/s/ecstatic-einstein-92efo0?file=/src/App.js
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Router from "./Router/Router";
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+export default function App() {
+  return (
+    <>
+    <Header/>
+      <Router />
+    <Footer/>
+    </>
+  );
+}
